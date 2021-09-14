07fe6280a15c5be8fd721982b6f48412da963543
jinzhi0123 <grinawkward@gmail.com>
2021-09-14 20:42:00 +0800

    Site updated: 2021-09-14 20:42:00

--- js/bbr_index.js
+++ js/bbr_index.js
@@ -11,7 +11,7 @@ if (pathName === '/') {
             var date = d.getFullYear()+'/'+(d.getMonth()+1)+'/'+d.getDate() +' '+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds()
             var dataTime = timeago.format(date, 'zh_CN');
             var newdataTime = '<span class="datatime">'+dataTime+'</span>'
-            bberHtml += '<li class="item item-'+(i+1)+'">'+'<a href="https://jinzhi0123.gitee.io/bber/" style="color: var(--font-color);/*font-weight: normal;*/">'+newdataTime+'： '+urlToLink(item.content)+'</a></li>'
+            bberHtml += '<li class="item item-'+(i+1)+'">'+'<a href="https://jinzhi0123.gitee.io/bber" style="color: var(--font-color);/*font-weight: normal;*/">'+newdataTime+'： '+urlToLink(item.content)+'</a></li>'
           });
           $('#bber-talk').append('<span class="index-talk-icon" style="margin-right: 5px"><svg viewBox="0 0 550 550" width="15" height="15"><path d="M448 0H64C28.7 0 0 28.7 0 64v288c0 35.3 28.7 64 64 64h96v84c0 7.1 5.8 12 12 12 2.4 0 4.9-.7 7.1-2.4L304 416h144c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64zm16 352c0 8.8-7.2 16-16 16H288l-12.8 9.6L208 428v-60H64c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16h384c8.8 0 16 7.2 16 16v288z"></path></svg></span><ul class="talk-list">'+bberHtml+'</ul>')
         });
